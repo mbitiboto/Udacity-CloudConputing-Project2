16731d7dfbdec3b9cf71f5c640b9c0f968adcd23
bitiboto <mbitiboto@gmail.com>
2021-01-04 17:39:20 +0100

    successfull dployed codes

---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  "aws": {
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+  }
+}
